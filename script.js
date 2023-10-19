53b0bd828427befa85685bdef0433d548869ebc5
Lil-YH <73232658+Lil-YH@users.noreply.github.com>
2023-10-19 21:23:48 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+import { config } from "dotenv"
+config()
+
+import { OpenAI } from "openai"
+import { stdout } from "process";
+import readline from "readline"
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY
+});
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: stdout,
+});
+
+userInterface.prompt();
+userInterface.on("line", async input => {
+    const res = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: input}]
+    })
+    console.log(res.choices[0].message.content);
+    userInterface.prompt();
+});
+
