bdecafda1883da16dee3d92ffaceaadef0731095
Lil-YH <73232658+Lil-YH@users.noreply.github.com>
2023-10-20 00:09:30 +0800

    Fine tuned model and report generation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LLM-GUI
+
+A basic CLI based symptom collection bot built on OpenAIApi using a finetuned chat-gpt-3.5-turbo model
--- script.js
+++ script.js
@@ -14,13 +14,45 @@ const userInterface = readline.createInterface({
     output: stdout,
 });
 
+let messages = new Array();
+
+let generateReport = async () => {
+    const completion = await openai.chat.completions.create({
+        messages: messages,
+        model: "ft:gpt-3.5-turbo-0613:personal::8BO6cC3R",
+      });
+    console.log("Report for attending doctor:\n\n" + completion.choices[0].message.content);
+}
+
+// Define the regular expression pattern
+const pattern = /assist with your diagnosis and treatment/i; // 'i' flag for case-insensitive matching
+
+messages.push({ role: "system", content: "You are a chatbot. A patient will be interacting with you. You are to lead the conversation with the patient in order to obtain details about their symptoms, medical history until you have determined to have collected enough relevant data. Do not provide any diagnosis or medical advice to the patient. After ending the conversation with the patient, generate a summary of the data you gathered and propose 3 most possible diagnoses with an accompanying confidence level. Start the conversation with this phrase: \"Hi! I'm MediBot. Before we start, could I have your name please?\". Always direct the conversation back to medical and do not respond to questions unrelated to the conversation." })
+
+const completion = await openai.chat.completions.create({
+    messages: messages,
+    model: "ft:gpt-3.5-turbo-0613:personal::8BO6cC3R",
+  });
+console.log(completion.choices[0].message.content);
+
+
 userInterface.prompt();
 userInterface.on("line", async input => {
+    messages.push({ role: "user", content: input});
     const res = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: input}]
+        model: "ft:gpt-3.5-turbo-0613:personal::8BO6cC3R",
+        messages: messages
     })
+    messages.push(res.choices[0].message);
     console.log(res.choices[0].message.content);
-    userInterface.prompt();
+
+    if(pattern.test(res.choices[0].message.content)) {
+        generateReport();
+        // console.log("Last Message");
+    } else {
+        // console.log("Conversation continuing...");
+        userInterface.prompt();
+    }
+
 });
 
